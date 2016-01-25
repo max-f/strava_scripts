bcc72fb5d35cd50eeb61ab8536135c9226f431b3
Max <maaax.fischer@gmail.com>
2016-01-25 23:54:37 +0100

    First commit

---
+++ riddensegment.py
@@ -0,0 +1,11 @@
+
+class RiddenSegment(object):
+
+    def __init__(self, id, name, athlete_count):
+        self.id = id
+        self.name = name
+        self.efforts = 1
+        self.athlete_count = athlete_count
+
+
+# vim: expandtab tabstop=4 shiftwidth=4 softtabstop=4
